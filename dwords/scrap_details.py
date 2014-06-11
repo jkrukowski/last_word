0290867427670e403a16e5fbf281b4f668a22c73
jankrukowski <jan.krukowski@tigerspike.com>
2014-06-11 10:18:41 +0200

    initial commit

---
+++ dwords/scrap_details.py
@@ -0,0 +1,47 @@
+import urllib2
+import pickle
+from bs4 import BeautifulSoup
+import re
+
+
+def get_statement(url):
+    content = get_url_content(url)
+    soup = BeautifulSoup(content)
+    body = soup.find('div', id='body')
+    lstatement = body.find('p', text=re.compile('Last Statement'))
+    if lstatement:
+        lstatement = lstatement.find_all_next('p')
+        return lstatement
+
+
+def parse_statement(value):
+    return ' '.join((i.get_text() for i in value))
+
+
+def get_url_content(url):
+    f = urllib2.urlopen(url)
+    content = f.read()
+    f.close()
+    return content
+
+
+def get_table():
+    return pickle.load(open('table.pkl'))
+
+
+def parse_items():
+    result = []
+    table = get_table()
+    for data in table:
+        d = dict(data)
+        stm = get_statement(data['stm_link'])
+        d['stm'] = parse_statement(stm)
+        result.append(d)
+        print d
+    return result
+
+
+if __name__ == '__main__':
+    items = parse_items()
+    pickle.dump(items, open('full_table.pkl', 'wb'))
+    print '--- end'
---
+++ dwords/scrap_table.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+from itertools import izip
+import pickle
+from bs4 import BeautifulSoup
+
+BASE_URL = r'http://www.tdcj.state.tx.us/death_row/'
+HREFS_URL = r'http://www.tdcj.state.tx.us/death_row/dr_executed_offenders.html'
+DATE_FORMAT = '%m/%d/%Y'
+FILE_PATHS = ('/Users/micazook/Desktop/dr_info.html', '/Users/micazook/Desktop/dr_info2.html')
+
+parse_map = {
+    'lp': lambda x: int(x.get_text()),
+    'info_link': lambda x: BASE_URL + x.find('a')['href'].strip(),
+    'stm_link': lambda x: BASE_URL + x.find('a')['href'].strip(),
+    'surename': lambda x: x.get_text().strip(),
+    'name': lambda x: x.get_text().strip(),
+    'no': lambda x: int(x.get_text()),
+    'age': lambda x: int(x.get_text()),
+    'date': lambda x: datetime.strptime(x.get_text(), DATE_FORMAT),
+    'race': lambda x: x.get_text().strip(),
+    'county': lambda x: x.get_text().strip()
+}
+
+parse_keys = ('lp',
+              'info_link',
+              'stm_link',
+              'surename',
+              'name',
+              'no',
+              'age',
+              'date',
+              'race',
+              'county')
+
+
+def parse_statement(content):
+    if content:
+        return ' '.join((i.get_text() for i in content))
+    return None
+
+
+def get_file_content(path):
+    with open(path, 'rb') as f:
+        return f.read()
+
+
+def parse_row_item(item):
+    result = {}
+    for i in izip(parse_keys, item):
+        key, val = i
+        fn = parse_map[key]
+        result[key] = fn(val)
+    return result
+
+
+def get_rows():
+    result = []
+    for path in FILE_PATHS:
+        soup = BeautifulSoup(open(path))
+        table = soup.find('table')
+        to_iterate = table.find_all('tr')
+        for item in to_iterate:
+            to_parse = (td_item for td_item in item.find_all('td'))
+            result.append(parse_row_item(to_parse))
+    return result
+
+if __name__ == '__main__':
+    pickle.dump(get_rows(), open('table.pkl', 'wb'))
